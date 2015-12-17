4d243f2efdfd51571f12d2b5666d6912a5695312
Philippe Aka <philippeaka@outlook.com>
2015-12-17 18:51:18 -0500

    created admin layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController 
 
+  layout 'admin', only: :test 
+  
   def index 
   end 
+
+  def test 
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations"}
   get 'home/index'
+  get 'home/test'
 
   root to: 'home#index'
   resources :profiles
