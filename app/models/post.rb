dc4b2544c22ccbb4e3c3cd75320ec36cbda2a012
Philippe Aka <philippeaka@outlook.com>
2015-12-22 09:54:57 -0500

    trying to solve friendly id slug issue on production

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   belongs_to :user
 
   extend FriendlyId
-  friendly_id :title, use: :slugged
+  friendly_id :title, use: [:slugged, :finders]
   validates_presence_of :title, :body
 end
