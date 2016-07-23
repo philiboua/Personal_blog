4ab84b2af6247ea525b9037d4aa9ff42c77274c5
Philippe Aka <philippeaka@outlook.com>
2016-07-23 17:03:31 -0400

    created seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'faker'
+
+50.times do
+    post = Post.create!(
+    title:     Faker::Hipster.sentence + "akwajob",
+    body:    Faker::Hipster.paragraphs
+   )
+ end
+ posts = Post.all
