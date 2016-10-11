0a08b47be224d3e87c18207fa238698e61b85d80
Philippe Aka <philippeaka@outlook.com>
2016-10-11 02:10:13 -0400

    updated colors

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,22 @@
 //
 //= require jquery
 //= require bootstrap-sprockets
+//= require fakeloader
 //= require jquery_ujs
-//= require turbolinks
 //= require_tree .
 
+
+
+jQuery(document).ready(function($){
+  $("#fakeLoader").fakeLoader({
+    
+            timeToHide:1500, //Time in milliseconds for fakeLoader disappear
+            zIndex:999, // Default zIndex
+            spinner:"spinner1",//Options: 'spinner1', 'spinner2', 'spinner3', 'spinner4', 'spinner5', 'spinner6', 'spinner7' 
+            bgColor:"#ea0001", //Hex, RGB or RGBA colors
+            
+    });
+
+});
 
 
